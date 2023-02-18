ef9540e4b3c22dea2508e18195122f34c7ff9c75
Kumar <krishav66@gmail.com>
2023-02-18 13:58:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'e337599ec2mshbe510629c274344p14c594jsn9c89111caa16',
+      'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+  };
+
+  const getWeather = (city) => {
+    cityName.innerHTML = city
+  fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city , options)
+    .then(response => response.json())
+    .then(response => {console.log(response)
+    cloud_pct.innerHTML=response.cloud_pct
+        feels_like.innerHTML=response.feels_like
+        humidity.innerHTML=response.humidity
+        max_temp.innerHTML=response.max_temp
+        min_temp.innerHTML=response.min_temp
+        sunrise.innerHTML=response.sunrise
+        sunset.innerHTML=response.sunset
+        temp.innerHTML=response.temp
+        wind_speed.innerHTML=response.wind_speed})
+    .catch(err => console.error(err));
+  }
+  submit.addEventListener('click', (e) => {e.preventDefault();
+  getWeather(city.value)})
